41942540939c796314c5e53280665e8bf258ca56
Lawrence <lawrence@acode.ninja>
2023-01-27 11:25:17 +0000

    Add types export to scanner module

--- scanners/npm-audit/src/main.js
+++ scanners/npm-audit/src/main.js
@@ -1,18 +1,36 @@
-const {name, version} = require('../package.json');
+const packageJson = require('../package.json');
 const {readFile} = require('fs/promises');
 
 const Dockerfile = require('./assets/Dockerfile');
 const Scan = require('./assets/scan.sh');
 
-/**
- * @type {Scanner}
- */
 const scanner = {
-  name,
-  version,
+  /**
+   * @type {string}
+   */
+  name: packageJson.name,
+  /**
+   * @type {string}
+   */
+  version: packageJson.version,
+  /**
+   * @type {string}
+   */
   slug: 'npm-audit',
+  /**
+   * @type {{files: {[name: string]: string}}}
+   */
   buildConfiguration: {files: {Dockerfile, 'scan.sh': Scan}},
-  validate: async (configuration) => {},
+  /**
+   * @param {{name: string; target?: string;}} configuration
+   * @return {Promise<void>}
+   */
+  validate: async (configuration) => {
+  },
+  /**
+   * @param {string} location
+   * @return {Promise<{counts: *, issues: {name: *, description: *, type: string}[]}>}
+   */
   report: async (location) =>
     readFile(location)
       .then(content => content.toString('utf-8'))
