113bea3757101e5d9c5c241374893bfd86c7ab84
Lawrence <lawrence@acode.ninja>
2023-03-13 12:39:03 +0000

    Add missing YAML config fixture

--- application/tests/fixtures/Configuration.ts
+++ application/tests/fixtures/Configuration.ts
@@ -1,3 +1,8 @@
 export const JSONConfiguration = Buffer.from(JSON.stringify({
   scanners: ['test-scanner'],
 }));
+
+export const YAMLConfiguration = Buffer.from(`
+scanners:
+  - test-scanner
+`);
