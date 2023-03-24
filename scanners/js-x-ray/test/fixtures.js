23f72be5fbb543e8b575e5393766c30238102eb7
Lawrence <lawrence@acode.ninja>
2023-03-24 09:45:55 +0000

    Fix linting errors

--- scanners/js-x-ray/test/fixtures.js
+++ scanners/js-x-ray/test/fixtures.js
@@ -7,23 +7,23 @@ module.exports = {
         description: 'A RegEx as been detected as unsafe and may be used for a ReDoS Attack.',
         extracts: [
           {
-            path: "node_modules/squirrelly/dist/browser/squirrelly.dev.js",
-            lines: ["141"],
-            language: "javascript",
-            code: "  var singleQuoteReg = /'(?:\\\\[\\s\\w\"'\\\\`]|[^\\n\\r'\\\\])*?'/g;\r"
+            path: 'node_modules/squirrelly/dist/browser/squirrelly.dev.js',
+            lines: ['141'],
+            language: 'javascript',
+            code: '  var singleQuoteReg = /\'(?:\\\\[\\s\\w"\'\\\\`]|[^\\n\\r\'\\\\])*?\'/g;\r',
           },
           {
-            path: "node_modules/squirrelly/dist/squirrelly.cjs.js",
-            lines: ["167"],
-            language: "javascript",
-            code: "var singleQuoteReg = /'(?:\\\\[\\s\\w\"'\\\\`]|[^\\n\\r'\\\\])*?'/g;\r"
+            path: 'node_modules/squirrelly/dist/squirrelly.cjs.js',
+            lines: ['167'],
+            language: 'javascript',
+            code: 'var singleQuoteReg = /\'(?:\\\\[\\s\\w"\'\\\\`]|[^\\n\\r\'\\\\])*?\'/g;\r',
           },
           {
-            path: "node_modules/squirrelly/dist/squirrelly.es.js",
-            lines: ["163"],
-            language: "javascript",
-            code: "var singleQuoteReg = /'(?:\\\\[\\s\\w\"'\\\\`]|[^\\n\\r'\\\\])*?'/g;\r"
-          }
+            path: 'node_modules/squirrelly/dist/squirrelly.es.js',
+            lines: ['163'],
+            language: 'javascript',
+            code: 'var singleQuoteReg = /\'(?:\\\\[\\s\\w"\'\\\\`]|[^\\n\\r\'\\\\])*?\'/g;\r',
+          },
         ],
       },
     ],
