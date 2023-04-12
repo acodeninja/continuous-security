3a329f21ea53894cfa8f4b139b69de237491f953
Lawrence <lawrence@acode.ninja>
2023-04-12 15:23:42 +0100

    Output scan when in debug mode

--- application/src/Scan.ts
+++ application/src/Scan.ts
@@ -22,6 +22,8 @@ export class Scan {
     this.imageHash = await buildImage(this.scanner.buildConfiguration);
     this.output = await makeTemporaryFolder(`${this.scanner.slug}-`);
 
+    if (process.env.DEBUG) this.emitter.emit('debug', `${this.scanner.name} output at ${this.output}`);
+
     this.emitter.emit('scan:setup:finished', this.scanner.name);
 
     return this;
