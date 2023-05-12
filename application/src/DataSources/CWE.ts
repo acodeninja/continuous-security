a067d59f17688f528a7db18439bdd4f779dc6033
Lawrence <lawrence@acode.ninja>
2023-05-12 09:57:30 +0100

    Include CWE specific data output

--- application/src/DataSources/CWE.ts
+++ application/src/DataSources/CWE.ts
@@ -15,7 +15,7 @@ export class CWE {
   getById(id: string, issue?: ScanReportIssue): ReportOutputIssueReference {
     const parsedId = id.toLowerCase().replace('cwe-', '');
 
-    const [weakness] = select(`//*[local-name(.)='Weakness' and @ID='${parsedId}']`, this.data);
+    const [weakness] = select(`//*[local-name(.)='Weakness' and @id='${parsedId}']`, this.data);
 
     if (weakness) {
       const queryable = XmlQuery(XmlReader.parseSync(weakness.toString()));
@@ -31,16 +31,40 @@ export class CWE {
       return {
         label: id,
         title: attributes.Name,
-        description: queryable.find('Description')
-          .first()
-          .text()
-          .replace(/(\r\n|\n|\r)/gm, ' ')
-          .replace(/\s+/g, ' ')
-          .trim(),
+        description: this.tidyString(queryable.find('Description').first().text()),
         url: `https://cwe.mitre.org/data/definitions/${parsedId}.html`,
+        dataSourceSpecific: {
+          cwe: {
+            extendedDescription: this.tidyString(queryable.find('Extended_Description').first().text()),
+            background: this.tidyString(queryable.find('Background_Detail').first().text()),
+            consequences: queryable.find('Consequence').map(node => {
+              node = XmlQuery(node);
+              return {
+                scope: this.tidyString(node.find('Scope').text()),
+                impact: this.tidyString(node.find('Impact').text()),
+                note: this.tidyString(node.find('Note').text()),
+              };
+            }),
+            mitigations: queryable.find('Mitigation').map(node => {
+              node = XmlQuery(node);
+              return {
+                phase: this.tidyString(node.find('Phase').text()),
+                description: this.tidyString(node.find('Description').text()),
+                effectiveness: this.tidyString(node.find('Effectiveness').text()),
+                notes: this.tidyString(node.find('Effectiveness_Notes').text()),
+              };
+            }),
+          }
+        }
       };
     }
 
     throw new CWEDoesNotExist();
+  }
+
+  private tidyString(input: string) {
+    return input.replace(/(\r\n|\n|\r)/gm, ' ')
+      .replace(/\s+/g, ' ')
+      .trim()
   }
 }
