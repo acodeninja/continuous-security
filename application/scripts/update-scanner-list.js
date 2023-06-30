1f885491775a0fe6071f792526d19a2f7a89bc38
Lawrence <lawrence@acode.ninja>
2023-07-01 00:25:39 +0100

    Add update-scanner-list script

---
+++ application/scripts/update-scanner-list.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const {readdir, writeFile} = require('fs/promises');
+const {resolve} = require('path');
+
+readdir(resolve(process.cwd(), '..', 'scanners'))
+  .then(l => l.filter(i => i !== '_base'))
+  .then(l => Promise.all(l.map(async (name) => {
+    const path = resolve(process.cwd(), '..', 'scanners', name, 'package.json');
+    const {description} = require(path);
+
+    return {
+      name,
+      description,
+      package: `@continuous-security/${name}`,
+    };
+  })))
+  .then(scanners => ({scanners}))
+  .then(scannerData => writeFile(
+    resolve(process.cwd(), 'src', 'scanners.json'),
+    JSON.stringify(scannerData, null, 2),
+  ))
+  .catch(console.error)
