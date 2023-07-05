4468e899144ef7c5cdccf3780ab323da499a59ad
Lawrence <lawrence@acode.ninja>
2023-07-05 08:24:41 +0100

    Handle string via contents for npm audit

--- scanners/javascript-npm-audit/test/integration.test.ts
+++ scanners/javascript-npm-audit/test/integration.test.ts
@@ -10,6 +10,15 @@ setupIntegrationTests(
     scanner: '@continuous-security/scanner-javascript-npm-audit',
     issues: [
       {
+        description: 'semver vulnerable to Regular Expression Denial of Service',
+        fix: 'Upgrade to version above <7.5.2',
+        package: {name: 'semver'},
+        references: ['CWE-1333', 'GHSA-c2qf-rxjj-qqgw'],
+        severity: 'moderate',
+        title: 'Vulnerable Third-Party Library `semver`',
+        type: 'dependency',
+      },
+      {
         description: 'Insecure template handling in Squirrelly',
         fix: 'Upgrade to version above <=8.0.8',
         package: {name: 'squirrelly'},
