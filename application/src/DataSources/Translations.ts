acd6b74ed5fd36c645a69145ee2a50c098a1d7d9
Lawrence <lawrence@acode.ninja>
2023-07-15 14:26:42 +0100

    Do not replace where string is part of a word

--- application/src/DataSources/Translations.ts
+++ application/src/DataSources/Translations.ts
@@ -1,12 +1,24 @@
 export const terms: Record<string, string> = {
   'black box': 'closed box',
+  'black hat': 'attacker',
   'black list': 'block list',
+  'man in the middle': 'adversary-in-the-middle',
+  'sanity check': 'coherence check',
   'white box': 'open box',
+  'white hat': 'offensive security researcher',
   'white label': 'unlabeled',
   'white list': 'allow list',
   abort: 'cancel',
+  cripple: 'degrade',
+  disable: 'deactivate',
+  he: 'they',
   master: 'main',
+  segregate: 'separate',
+  she: 'they',
+  man: 'person',
+  woman: 'person',
   slave: 'subordinate',
+  tribe: 'team',
 };
 
 export const getAllTerms = (): Array<Array<string>> => Object.entries(terms)
@@ -24,7 +36,7 @@ export const getAllTerms = (): Array<Array<string>> => Object.entries(terms)
 
 export const translate = (input: string): string => {
   getAllTerms().forEach(([find, replace]) => {
-    input = input.replace(new RegExp(find, 'gi'), (substring): string => {
+    input = input.replace(new RegExp(`\\b${find}\\b`, 'gmi'), (substring): string => {
       if (substring[0] === substring[0].toLowerCase()) return replace;
       return `${replace[0].toUpperCase()}${replace.slice(1)}`;
     });
