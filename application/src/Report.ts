d7436135cda19cf9eb6adedf6bccc0a07f6b62d6
Lawrence <lawrence@acode.ninja>
2023-07-30 09:35:20 +0100

    Cache references between lookups to remove fetching loop

--- application/src/Report.ts
+++ application/src/Report.ts
@@ -14,6 +14,7 @@ export class Report {
   private cveDataset: CVE = new CVE();
   private cweDataset: CWE = new CWE();
   private osvDataset: OSV = new OSV();
+  private cachedReferences: Record<string, ReportOutputIssueReference>;
   private cached: ReportOutput;
   private emitter: Emitter;
   private severityOrder = ['critical', 'high', 'moderate', 'low', 'info', 'unknown'];
@@ -51,40 +52,50 @@ export class Report {
 
   private async expandReferences(references: Array<string>):
     Promise<Array<ReportOutputIssueReference>> {
-    const expandedReferences: Record<string, ReportOutputIssueReference> = {};
+    const expandedReferences: Record<string, ReportOutputIssueReference> = {...this.cachedReferences};
     const referencesToProcess = Array.from(references);
+    const processedReferences = [];
 
-    while (referencesToProcess
-      .filter(r => !Object.keys(expandedReferences).includes(r)).length > 0) {
-      for (const ref of referencesToProcess
-        .filter(r => !Object.keys(expandedReferences).includes(r))) {
-        const slug = ref.toLowerCase();
-        this.emitter.emit('report:reference', `fetching details for ${ref}`);
+    while (referencesToProcess.length > 0) {
+      const ref = referencesToProcess.pop();
 
-        try {
-          if (slug.indexOf('cwe') === 0) {
-            expandedReferences[ref] = this.cweDataset.getById(ref.toUpperCase());
-          } else if (slug.indexOf('cve') === 0) {
-            expandedReferences[ref] = await this.cveDataset.getById(ref.toUpperCase());
-          } else {
-            expandedReferences[ref] = await this.osvDataset.getById(ref);
-          }
-        } catch (e) {
-          this.emitter.emit(
-            'report:reference:failure',
-            `Failed to fetch vulnerability reference ${ref}`,
-          );
-          referencesToProcess.splice(referencesToProcess.indexOf(ref), 1);
+      if (!ref) continue;
+
+      processedReferences.push(ref);
+
+      if (!!expandedReferences[ref]) continue;
+
+      const slug = ref.toLowerCase();
+      this.emitter.emit('report:reference', `fetching details for ${ref}`);
+
+      try {
+        if (slug.indexOf('cwe') === 0) {
+          expandedReferences[ref] = this.cweDataset.getById(ref.toUpperCase());
+        } else if (slug.indexOf('cve') === 0) {
+          expandedReferences[ref] = await this.cveDataset.getById(ref.toUpperCase());
+        } else {
+          expandedReferences[ref] = await this.osvDataset.getById(ref);
         }
+      } catch (e) {
+        this.emitter.emit(
+          'report:reference:failure',
+          `Failed to fetch vulnerability reference ${ref}`,
+        );
+      }
 
-        if (expandedReferences[ref]?.dataSourceSpecific?.osv?.aliases) {
-          expandedReferences[ref].dataSourceSpecific.osv.aliases
-            .forEach(a => referencesToProcess.push(a));
-        }
+      if (expandedReferences[ref]?.dataSourceSpecific?.osv?.aliases) {
+        expandedReferences[ref].dataSourceSpecific.osv.aliases
+          .forEach(a => {
+            if (!expandedReferences[ref]) referencesToProcess.push(a);
+            if (!processedReferences.includes(a)) referencesToProcess.push(a);
+          });
       }
     }
 
-    const outputReferences = Object.values(expandedReferences);
+    this.cachedReferences = expandedReferences;
+
+    const outputReferences = Object.values(this.cachedReferences)
+      .filter(r => processedReferences.includes(r.label));
 
     outputReferences.sort((a, b) =>
       (a.label < b.label) ? -1 : ((a.label > b.label) ? 1 : 0));
