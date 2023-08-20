ec325312f3c2f68467fc5bf001c89349f59d5f23
Lawrence <lawrence@acode.ninja>
2023-08-20 10:13:28 +0100

    Use fake date for all tests

--- application/jest.setup.js
+++ application/jest.setup.js
@@ -1,8 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {existsSync} = require('fs');
 
-// eslint-disable-next-line no-undef
-global.__non_webpack_require__ = jest.fn().mockImplementation((path) => {
-  if (existsSync(path)) return require(path);
-  return {default: ''};
-});
+module.exports = async () => {
+  /* eslint-disable no-undef */
+  jest.useFakeTimers();
+  jest.setSystemTime(new Date(2020, 3, 1, 1, 30, 10, 30));
+  jest.spyOn(Date.prototype, 'getTimezoneOffset').mockReturnValue(0);
+  /* eslint-enable no-undef */
+
+  // eslint-disable-next-line no-undef
+  global.__non_webpack_require__ = jest.fn().mockImplementation((path) => {
+    if (existsSync(path)) return require(path);
+    return {default: ''};
+  });
+};
